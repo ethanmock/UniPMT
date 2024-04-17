fc26213fef421e897a7cc4703b86717db8696ac6
ethanmock <yixin.su@outlook.com>
2024-04-17 22:23:45 +0800

    pt_zero

--- code/config/pmt.py
+++
@@ -1,9 +0,0 @@
-pm_weight = 0.0001
-pt_weight = 0.1
-pmt_weight = 0.2
-
-pt_neg_sample = 1 
-lr = 0.000001
-reg_lambda = 0.00001
-
-sample_m = True 
--- code/config/teim.py
+++
@@ -1,7 +0,0 @@
-pm_weight = 0.1
-pt_weight = 0.5
-pmt_weight = 0.1
-
-pt_neg_sample = 1
-
-sample_m = True 
